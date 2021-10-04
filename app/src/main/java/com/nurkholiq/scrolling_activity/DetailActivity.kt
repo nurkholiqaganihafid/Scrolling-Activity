fac588d89d156c7583b47b4e0e6cf483708b3ebb
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-04 15:06:50 +0700

    Modified beberapa file dengan Custom scrolling dan menampilkan data logcat

--- app/src/main/java/com/nurkholiq/scrolling_activity/DetailActivity.kt
+++ app/src/main/java/com/nurkholiq/scrolling_activity/DetailActivity.kt
@@ -1,11 +1,14 @@
 package com.nurkholiq.scrolling_activity
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
 import com.nurkholiq.scrolling_activity.databinding.ActivityDetailBinding
+import kotlin.math.abs
 
 class DetailActivity : AppCompatActivity() {
 
@@ -26,6 +29,16 @@ class DetailActivity : AppCompatActivity() {
         }
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
+                // COLLAPSED KETIKA DI TUTUP
+                Log.e("appBarLayout", "collapsed")
+            } else {
+                // EXPANDED KETIKA DI BUKA
+                Log.e("appBarLayout", "expanded")
+            }
+        })
 
     }
 
