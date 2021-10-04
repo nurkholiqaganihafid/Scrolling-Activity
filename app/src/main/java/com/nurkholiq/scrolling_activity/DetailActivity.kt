a8231cfcb773d14be920848905e1463db8f6188a
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-04 15:26:39 +0700

    Modified DetailActivity.kt - Mengganti nama toolbarLayout dengan yang ada di myName collapsed

--- app/src/main/java/com/nurkholiq/scrolling_activity/DetailActivity.kt
+++ app/src/main/java/com/nurkholiq/scrolling_activity/DetailActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.material.appbar.AppBarLayout
 import com.nurkholiq.scrolling_activity.databinding.ActivityDetailBinding
 import kotlin.math.abs
@@ -20,23 +21,34 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-//        binding.toolbarLayout.title = title
-        binding.toolbarLayout.title = "Scrolling Activity"
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        setSupportActionBar(findViewById(R.id.toolbar))
+////        binding.toolbarLayout.title = title
+//        binding.toolbarLayout.title = "Scrolling Activity"
+//        binding.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
 
+        val myName = "Nurkholiq Agani Hafid"
+        val myRole = "Android"
+        val avatar = R.drawable.img_kotlin_android
+
+        setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        binding.imgAvatar.setImageDrawable(ContextCompat.getDrawable(this, avatar))
+        binding.tvTitle.text = myName
+        binding.tvRole.text = myRole
 
         binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
             if (abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
                 // COLLAPSED KETIKA DI TUTUP
                 Log.e("appBarLayout", "collapsed")
+                binding.toolbarLayout.title = myName
             } else {
                 // EXPANDED KETIKA DI BUKA
                 Log.e("appBarLayout", "expanded")
+                binding.toolbarLayout.title = ""
             }
         })
 
